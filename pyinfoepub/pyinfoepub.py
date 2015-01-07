440fb6d942aba4459346a27d3c798b9d9e45cf46
Cristian Năvălici <cristian.navalici@gmail.com>
2015-01-07 20:54:28 +0100

    minor actions on arg parse

--- pyinfoepub/pyinfoepub.py
+++ pyinfoepub/pyinfoepub.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(prog="PyInfoEpub", description='Extracts information from an epub file.')
+parser.add_argument('epub_file', metavar='file.epub', type=argparse.FileType('r'), default=sys.stdin, help='target epub file')
+parser.add_argument('--version', action='version', version='%(prog)s 0.2')
+
+args = parser.parse_args()
+
+with args.epub_file as epub_file:
+    print(epub_file.readlines())
