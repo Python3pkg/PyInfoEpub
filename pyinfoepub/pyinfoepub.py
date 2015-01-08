f7e8ee017cb8847d30df15320f169a5a3c7721c0
Cristian Năvălici <cristian.navalici@gmail.com>
2015-01-08 21:11:05 +0100

    work on rebuilding parsers

--- pyinfoepub/pyinfoepub.py
+++ pyinfoepub/pyinfoepub.py
@@ -3,11 +3,13 @@
 import argparse
 import sys
 
+from lib.info_epub import PyInfoEpub
+
 parser = argparse.ArgumentParser(prog="PyInfoEpub", description='Extracts information from an epub file.')
 parser.add_argument('epub_file', metavar='file.epub', type=argparse.FileType('r'), default=sys.stdin, help='target epub file')
 parser.add_argument('--version', action='version', version='%(prog)s 0.2')
 
 args = parser.parse_args()
 
-with args.epub_file as epub_file:
-    print(epub_file.readlines())
+pyob = PyInfoEpub(args.epub_file.name)
+pyob.get_info()
